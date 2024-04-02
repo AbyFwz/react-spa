75b6425f219bb3c90386f823b825ecd6d55c4834
abyfwz <abyanfawwaz77@gmail.com>
2024-04-03 02:14:52 +0700

    UI (Card): Remove description from card.

--- src/components/card/Cards.js
+++ src/components/card/Cards.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Card, Button } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom'
 
-function Cards({id, image, name, status, species, gender, originName, originUrl, locationName, locationUrl}) {
+function Cards({id, image, name}) {
     const navigate = useNavigate();
     return (
         <>
@@ -10,13 +10,6 @@ function Cards({id, image, name, status, species, gender, originName, originUrl,
         <Card.Img variant="top" src={image} />
         <Card.Body>
             <Card.Title>{name}</Card.Title>
-            <Card.Text>
-                <span>Status: {status}<br/></span>
-                <span>Species: {species}<br/></span>
-                <span>Gender: {gender}<br/></span>
-                <span>Origin: {originName}<br/></span>
-                <span>Origin: {locationName}<br/></span>
-            </Card.Text>
             <Button variant="primary" onClick={() => {navigate(`/character-details/${id}`)}}>Character Details</Button>
         </Card.Body>
         </Card>
