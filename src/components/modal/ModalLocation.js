43e67a96692501e3dbca5fec57bb9b8a11e2bc6c
abyfwz <abyanfawwaz77@gmail.com>
2024-04-03 04:58:25 +0700

    Feat (Component): Add modal to add location.

--- src/components/modal/ModalLocation.js
+++ src/components/modal/ModalLocation.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import useFetch from '../../hooks/useFetch';
-import { Button, Modal } from 'react-bootstrap';
+import { Button, Form, Modal } from 'react-bootstrap';
+import FormSelect from '../form/FormSelect';
 
 function ModalLocation({show, handleClose}) {
     const [data, setData] = useState([]);
@@ -13,26 +14,30 @@ function ModalLocation({show, handleClose}) {
     }, []);
     return (
         <>
-        <Modal
-        show={show}
-        onHide={handleClose}
-        backdrop="static"
-        keyboard={false}
-      >
-        <Modal.Header closeButton>
-          <Modal.Title>Modal title</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          I will not close if you click outside me. Don't even try to press
-          escape key.
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary">Understood</Button>
-        </Modal.Footer>
-      </Modal>
+            <Modal
+                show={show}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Add Character to Spesific Location</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Label>Select Location</Form.Label>
+                            <FormSelect />
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="success">Submit</Button>
+                </Modal.Footer>
+            </Modal>
         </>
     )
 }
