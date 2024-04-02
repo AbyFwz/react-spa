c3a5ba8c1865b87262ae593d98538b6e906af08d
abyfwz <abyanfawwaz77@gmail.com>
2024-04-03 04:26:03 +0700

    UI (Layout): Make a grid layout on index.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,56 @@
+/* React Mandatory */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { createBrowserRouter, RouterProvider, Router, Route, Routes, BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+/* Stylesheet */
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+/* Component */
+import CharacterList from './pages/character-list/';
+import CharacterDetail from './pages/character-detail/';
+import CharacterLocation from './pages/character-by-location/';
+import Header from './components/header';
+import Footer from './components/footer';
+import { Col, Container, Row } from 'react-bootstrap';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// const router = createBrowserRouter([
+//   {
+//       path: "/",
+//       element: <CharacterList />
+//   },
+//   {
+//       path: "/character-details",
+//       element: <CharacterDetail />
+//   },
+//   {
+//       path: "/character-locations",
+//       element: <CharacterLocation />
+//   }
+// ]);
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Container>
+        <Row>
+          <Col><Header /></Col>
+        </Row>
+        <Row>
+          <Col xs="auto" sm="auto" md="auto" lg="auto" xl="auto">
+              <Routes>
+                <Route path="/" element={<CharacterList />} />
+                <Route path="/character-details/:id" element={<CharacterDetail />} />
+                <Route path="/character-locations" element={<CharacterLocation />} />
+                <Route path="/character-locations.details/:id" element={<CharacterLocation />} />
+              </Routes>
+          </Col>
+        </Row>
+        <Row>
+          <Col><Footer /></Col>
+        </Row>
+      </Container>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
