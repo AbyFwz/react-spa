ac92ccdee2e70e5a30f3d3725fc328cc26915cf4
abyfwz <abyanfawwaz77@gmail.com>
2024-04-03 04:26:24 +0700

    UI (Loading): Remove loading text.

--- src/components/loading/Loading.js
+++ src/components/loading/Loading.js
@@ -3,9 +3,7 @@ import { Spinner } from 'react-bootstrap'
 
 function Loading() {
     return (
-        <Spinner animation="border" role="status">
-            <span className="sr-only">Loading...</span>
-        </Spinner>
+        <Spinner animation="border" role="status" />
     )
 }
 
