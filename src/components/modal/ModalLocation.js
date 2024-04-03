8c2632f420283d9d1c20b494267fd0deb4dd78f7
abyfwz <abyanfawwaz77@gmail.com>
2024-04-03 07:42:54 +0700

    Feat (Modal): Add set data function after fill the form

--- src/components/modal/ModalLocation.js
+++ src/components/modal/ModalLocation.js
@@ -1,17 +1,40 @@
 import React, { useEffect, useState } from 'react'
 import useFetch from '../../hooks/useFetch';
-import { Button, Form, Modal } from 'react-bootstrap';
-import FormSelect from '../form/FormSelect';
+import { Alert, Button, Form, Modal } from 'react-bootstrap';
+import { useLocalStorage, useSetLocalStorage } from '../../contexts/LocalStorageContext';
+import { useParams } from 'react-router-dom';
 
 function ModalLocation({show, handleClose}) {
+    const { id } = useParams();
     const [data, setData] = useState([]);
+    const [location, setLocation] = useState('');
+    const [showPopup, setShowPopup] = useState(false);
+    const { storedData } = useLocalStorage();
+    const setStoredData = useSetLocalStorage();
     const fetch = useFetch();
 
-    useEffect(() => {
-        fetch({url:"https://rickandmortyapi.com/api/location", method:"GET" }).then((resp) => {
-            console.log(resp);
+    const handleFetch = () => {
+        fetch(
+            {url: "https://rickandmortyapi.com/api/location", method:"GET"}
+        ).then((resp) => {
+            setData(resp.data.results);
+        }).catch((err) => {
+            setData([]);
         });
-    }, []);
+    }
+
+    useEffect(() => {
+        handleFetch();
+    }, [])
+
+    const handleSetData = () => {
+        setStoredData(id, location);
+        setShowPopup(true);
+        setTimeout(() => {
+            handleClose();
+        }, 1500);
+    }
+
     return (
         <>
             <Modal
@@ -20,14 +43,21 @@ function ModalLocation({show, handleClose}) {
                 backdrop="static"
                 keyboard={false}
             >
+                <Alert show={showPopup} variant="success" onClose={() => setShowPopup(false)}>
+                    Data submitted successfully!
+                </Alert>
                 <Modal.Header closeButton>
-                    <Modal.Title>Add Character to Spesific Location</Modal.Title>
+                    <Modal.Title>Add Character to Specific Location</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <Form>
                         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                             <Form.Label>Select Location</Form.Label>
-                            <FormSelect />
+                            <Form.Select aria-label="Default select example" onChange={(e) => setLocation(e.target.value)}>
+                                {data?.map((item) => {
+                                    return (<option key={item.id} value={item.id}>{item.name}</option>)
+                                })}
+                            </Form.Select>
                         </Form.Group>
                     </Form>
                 </Modal.Body>
@@ -35,7 +65,7 @@ function ModalLocation({show, handleClose}) {
                     <Button variant="secondary" onClick={handleClose}>
                         Close
                     </Button>
-                    <Button variant="success">Submit</Button>
+                    <Button variant="success" onClick={handleSetData}>Submit</Button>
                 </Modal.Footer>
             </Modal>
         </>
